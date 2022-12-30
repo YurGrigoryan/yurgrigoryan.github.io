5d57197e00d26d272517c8aa4edec7c76f29a5e4
Yura Grigoryan <118906734+YurGrigoryan@users.noreply.github.com>
2022-12-30 10:19:21 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let x = document.querySelector(".englishButton")
+let y = document.querySelector(".rusButton")
+let w = document.querySelector(".aboutH1")
+let q = document.querySelector(".yuraH1")
+let o = document.querySelector(".harc1")
+let l = document.querySelector(".harc2")
+let m = document.querySelector(".harc3")
+let n = document.querySelector(".harc4")
+let g = document.querySelector(".patasxan1")
+let b = document.querySelector(".patasxan2")
+let k = document.querySelector(".patasxan3")
+let f = document.querySelector(".patasxan4")
+let j = document.querySelector(".aboutMeH2")
+let z = document.querySelector(".myH1")
+
+x.addEventListener("click" , function(){
+    w.innerText = "ABOUT ME"
+    q.innerText = "YURA GRIGORYAN"
+    o.innerText = "Born"
+    l.innerText = "Country"
+    m.innerText = "Profession"
+    n.innerText = "Studied"
+    g.innerText = "January 13, 2010, Verin Artashat, Armenia"
+    b.innerText = "Armenia"
+    k.innerText = "Programmer"
+    f.innerText = "At It Artashat"
+    j.innerText = "My name is Yura Grigoryan.I from Armenia,from Verin Artashat.I am 12.I learn programming 7 month, at It Artashat․ My dream is to become one of the best programmers in the world. I love games very much. That's why I decided that I should become a programmer.I made games and programs.I learned html,css,js."
+    z.innerTxet = "My Games"
+})
+y.addEventListener("click" , function(){
+    w.innerText = "ОБО МНЕ"
+    q.innerText = "ЮРА ГРИГОРЯН"
+    o.innerText = "Родился"
+    l.innerText = "Страна"
+    m.innerText = "Профессия"
+    n.innerText = "Учился"
+    g.innerText = "13 января 2010, Верин Арташат, Армения"
+    b.innerText = "Армения"
+    k.innerText = "Программист"
+    f.innerText = "в It Artashat"
+    j.innerText = "Меня зовут Юра Григорян. Я из Армении, из Верин Арташат. Мне 12. Изучаю программирование 7 месяцев, в Ит Арташат․ Моя мечта — стать одним из лучших программистов в мире. Я очень люблю игры. Вот почему я решил, что должен стать программистом. Я делал игры и программы. Я изучил html, css, js."
+    z.innerTxet = "Мои игры"
+})
